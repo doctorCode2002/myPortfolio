580fc16eddff7b4b7f6b5903fa981f16d4dd94ef
doctorCode2002 <mohammeda.abutaleb@gmail.com>
2026-08-15 14:19:23 +0300

    feat: add sitemap and robots.txt metadata routes
    Introduces src/lib/site.js as the single SITE_URL/person-identity
    source of truth and generates /sitemap.xml and /robots.txt from it
    under the static export.

---
+++ src/lib/site.js
@@ -0,0 +1,12 @@
+export const SITE_URL = "https://mohammedabutaleb.com";
+
+export const PERSON = {
+  name: "Mohammed Ashraf",
+  jobTitle: "Full-Stack Developer",
+  sameAs: [
+    "https://github.com/doctorCode2002",
+    "https://www.linkedin.com/in/mohammed2002",
+    "https://www.instagram.com/mohammedabu.taleb/",
+  ],
+  knowsAbout: ["React", "Tailwind CSS", "GSAP", "JavaScript"],
+};
